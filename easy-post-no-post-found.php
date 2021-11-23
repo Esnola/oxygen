091b0379a57ab4b9fc295bde1dc26d538b18d0f0
alexsoluweb <94582929+alexsoluweb@users.noreply.github.com>
2021-11-23 16:10:47 -0500

    Update easy-post-no-post-found.php

--- easy-post-no-post-found.php
+++ easy-post-no-post-found.php
@@ -1,15 +1,22 @@
 <?php
-	add_filter("oxygen_vsb_after_component_render", "asw_oxygen_vsb_after_component_render", 100, 1);
+/*
+* Description: If empty post for the actual query in the easy post module (usefull for query outside the main loop)
+* Return: 
+* 	if no posts in the query: Desired Output
+* 	Else: the posts
+*/
 
-	if (!function_exists('asw_oxygen_vsb_after_component_render')) {
-		function asw_oxygen_vsb_after_component_render($outputContent){		
-			$pattern = '/\boxy-post\b/i';
-			if(!preg_match($pattern , $outputContent)) {
-				return "<p>No Posts</p>";
-			}
-			return $outputContent;
-		}	
-	}
+add_filter("oxygen_vsb_after_component_render", "asw_oxygen_vsb_after_component_render", 100, 1);
+
+if (!function_exists('asw_oxygen_vsb_after_component_render')) {
+	function asw_oxygen_vsb_after_component_render($outputContent){		
+		$pattern = '/\boxy-post\b/i';
+		if(!preg_match($pattern , $outputContent)) {
+			return "<p>No Posts</p>";
+		}
+		return $outputContent;
+	}	
+}
 
 	
 ?>
