c709ee31faa5700f51f4dbd084c9be1e376c85d8
alexsoluweb <94582929+alexsoluweb@users.noreply.github.com>
2021-11-23 13:11:31 -0500

    Update get_desired_size_feautured_image_url.php

--- get_desired_size_feautured_image_url.php
+++ get_desired_size_feautured_image_url.php
@@ -1,11 +1,11 @@
 <?php
 
 /* 
-* Description: Get the desired size featured image url
+* Description: Get the desired size featured image URL
 * $id: the attachment image id (not the post id), default: current loop post id
 * $size: the wanted dimension (thumbnail, medium,  medium_large, large, full), default: medium_large
 * Example of use to show only thumbnail: asw_get_desired_size_feautured_image_url(null, "thumbnail");
-* Return: Desired featured image url size, or placeholder image from https://via.placeholder.com API
+* Return: Desired featured image size URL, or placeholder image from https://via.placeholder.com API
 */
 function asw_get_desired_size_feautured_image_url($id=null, $size="medium_large"){
     $id = (isset($id))? get_the_ID(): $id;
